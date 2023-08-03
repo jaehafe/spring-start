e950281fa02b52465082c8029b685d295f6f26c9
Jaeha Lee <wogkdkrm112@gmail.com>
2023-08-03 21:52:19 +0900

    feat: 회원 save, findById, findByName, findAll

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -8,6 +8,7 @@ public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
+
     @Override
     public Member save(Member member) {
         member.setId(++sequence);
@@ -29,12 +30,8 @@ public class MemoryMemberRepository implements MemberRepository {
 
     @Override
     public List<Member> findAll() {
+        System.out.println(">>>"+ store.values());
         return new ArrayList<>(store.values());
     }
-
-    public void clearStore() {
-        store.clear();
-    }
-
-
 }
+
