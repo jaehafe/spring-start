e233047115556d83317633d2217e9cf66aa37efe
Jaeha Lee <wogkdkrm112@gmail.com>
2023-09-16 20:48:10 +0900

    feat: 회원 등록 기능

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,14 +1,36 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
 
     //해당 변수가 "변경할 수 없는(final)" 상태임, 불변성 보장
     private final MemberService memberService;
+    @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        System.out.println("memebr = " + member.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
